15394a369a5edfef75e61052dc4bc28cf77a33da
sigma-andex <77549848+sigma-andex@users.noreply.github.com>
2022-08-26 14:14:41 +0100

    Useless update

--- src/Fetch.purs
+++ src/Fetch.purs
@@ -42,6 +42,7 @@ import Prim.Row (class Union)
 -- |   { status, text } <- fetch requestUrl { headers: { "Accept": "application/json" }}
 -- |   responseBody <- text
 -- | ```
+-- | 
 -- | A more complex example doing a Post request:
 -- | ```purescript
 -- | do
